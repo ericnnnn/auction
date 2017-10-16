204aabdbf23bf311a476ffad70526043b8f442c9
Eric Li <Eric.Li@nafinc.com>
2017-10-16 07:35:00 -0700

    third finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +12,14 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import {Routes} from "@angular/router";
+
+const routeConfig: Routes=[
+  {path:'',component:HomeComponent},
+  {path:'product/:prodTitle',component:ProductDetailComponent}
+]
 
 @NgModule({
   declarations: [
@@ -17,10 +29,16 @@ import { StarsComponent } from './stars/stars.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarsComponent
+    StarsComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
