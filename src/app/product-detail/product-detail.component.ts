ba9e3410513a61ecba5bff6eaf18aa54d037c919
Eric Li <Eric.Li@nafinc.com>
2017-10-17 07:34:38 -0700

    c6

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -13,6 +13,10 @@ export class ProductDetailComponent implements OnInit {
 
   product:Product;
   comments:Comment[];
+  newRating:number=5;
+  newComment:string="";
+
+  isCommentHidden=true;
 
   constructor(private routeInfo:ActivatedRoute,
               private productService:ProductService
@@ -26,6 +30,22 @@ export class ProductDetailComponent implements OnInit {
     this.comments=this.productService.getCommentsForProductId(productId);
 
     //this.productTitle=this.routeInfo.snapshot.params["prodTitle"];
+  }
+
+
+
+  addComment(){
+    let comment=new Comment(0,this.product.id,new Date().toISOString(),"SomeOne",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+
+    let sum=this.comments.reduce((sum,comment)=>sum+comment.rating,0);
+    this.product.rating=sum/this.comments.length;
+
+    this.newComment=null;
+    this.newRating=5;
+    this.isCommentHidden=true;
+
+
   }
 
 }
