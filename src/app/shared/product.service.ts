f7c735715e3414c608073b16da1eed59b25b8e43
Eric Li <Eric.Li@nafinc.com>
2017-10-24 07:23:13 -0700

    c7

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -22,6 +22,10 @@ export class ProductService {
 
   constructor() { }
 
+  getAllCategories():string[]{
+    return ["electronics","hardware","software"];
+  }
+
   getProdcuts():Product[]{
     return this.products;
   }
